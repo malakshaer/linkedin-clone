0797b878a0c96039c1edda73c2bb059331dace86
malakshaer <shaermk06@gmail.com>
2022-10-17 06:56:16 +0300

    frontend: landing page added

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import LandingHeader from "../components/LandingHeader";
+import HeroSection from "../components/HeroSection";
+
+const LandingPage = () => {
+  return (
+    <Fragment>
+      <LandingHeader />
+      <HeroSection />
+    </Fragment>
+  );
+};
+
+export default LandingPage;
