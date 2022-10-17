a708d104caac7de7319500f566d0af2579e2a771
malakshaer <shaermk06@gmail.com>
2022-10-17 07:06:13 +0300

    frontend: notification component added

---
+++ frontend/src/components/Notification.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import "../styles/notification.css";
+
+const Notification = () => {
+  return (
+    <div>
+      <Header />
+      <div className="notification-container">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
