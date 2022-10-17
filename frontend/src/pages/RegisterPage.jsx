d694e811b549085bd17d20fa93bccaf07f6132aa
malakshaer <shaermk06@gmail.com>
2022-10-17 06:57:01 +0300

    frontend: register page added

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import RegisterHeader from "../components/RegisterHeader";
+import RegisterForm from "../components/RegisterForm";
+
+const RegisterPage = () => {
+  return (
+    <Fragment>
+      <RegisterHeader />
+      <RegisterForm />
+    </Fragment>
+  );
+};
+
+export default RegisterPage;
