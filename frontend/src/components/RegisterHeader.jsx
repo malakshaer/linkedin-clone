60ecb2913cb9b65aa66d127f7d2a0030deae0e24
malakshaer <shaermk06@gmail.com>
2022-10-17 07:04:51 +0300

    frontend: register header component added

---
+++ frontend/src/components/RegisterHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../assets/linkedin-logo.svg";
+
+const RegisterHeader = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default RegisterHeader;
