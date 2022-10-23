d80e4c18b5e75a31947c3d01b440899a37148d26
malakshaer <shaermk06@gmail.com>
2022-10-23 11:51:37 +0300

    frontend: jobs page modified

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import "../styles/job.css";
 import companyImage from "../assets/profile-image.jpg";
-import ApplyButton from "./ApplyButton";
+import Button from "./Button";
 
 const Jobs = () => {
   return (
@@ -17,7 +17,7 @@ const Jobs = () => {
               <span>Name</span>
             </div>
             <p>Text</p>
-            <ApplyButton />
+            <Button className="apply-btn" text={"Apply now"} />
           </div>
         </div>
       </div>
